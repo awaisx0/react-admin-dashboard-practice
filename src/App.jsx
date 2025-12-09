9fbce340ee6b9a6389460d26de6b6522be807111
Awais <mawaiskhera.674@gmail.com>
2025-12-09 10:02:03 +0500

    first commit + setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material";
+import "./App.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Button variant="contained">MUI working</Button>
+    </>
+  );
+}
+
+export default App;
